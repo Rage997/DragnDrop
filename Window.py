ee71e870905478069303397f6a3804ce8558eb77
Rage <zuppin@usi.ch>
2019-09-07 13:51:10 +0200

    Increased window size

--- Window.py
+++ Window.py
@@ -7,8 +7,9 @@ class MainWidget(QMainWindow):
     def __init__(self, parent=None):
         super(MainWidget, self).__init__(parent)
         self.setWindowTitle("DragnDrop files")
-        self.resize(720,480)
+        self.resize(480,480)
         self.setAcceptDrops(True)
+        self.start()
 
     def dragEnterEvent(self, event):
         if event.mimeData().hasUrls():
@@ -20,8 +21,11 @@ class MainWidget(QMainWindow):
         files = [(u.toLocalFile()) for u in event.mimeData().urls()]
         for f in files:
             print(f)
-  
+    
+    def start(self):
+        app = QApplication()
+        self.show() 
           
-app = QApplication(sys.argv)            
-window = MainWidget()
-window.show()
+# app = QApplication(sys.argv)            
+# window = MainWidget()
+# window.show()
